479be3a650000bedb2ba6f41ac54a704d352a6cc
Prateem Shrestha <prateems@gmail.com>
2018-06-01 05:30:49 -0400

    Helpful mixins.

---
+++ django_util/mixins/bootstrap_form_mixin.py
@@ -0,0 +1,15 @@
+class BootstrapFormMixin(object):
+    """
+    Sets class="form-control" on all form fields except checkboxes and radios,
+    for which it applies class="form-check-input".
+
+    See: https://getbootstrap.com/docs/4.1/components/forms/
+    """
+    def __init__(self, *args, **kwargs):
+        super(BootstrapFormMixin, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            widget = self.fields[field_name].widget
+            if hasattr(widget, 'input_type') and (widget.input_type == 'checkbox' or widget.input_type == 'radio'):
+                widget.attrs.update({'class': 'form-check-input'})
+            else:
+                widget.attrs.update({'class': 'form-control'})
---
+++ django_util/mixins/slugged_model_mixin.py
@@ -0,0 +1,48 @@
+from django.template.defaultfilters import slugify
+from django.core.exceptions import FieldError
+import math
+
+
+class SluggedMixin:
+    """
+    Creates a slug from _slugify='title' and stores it in _slug_field='slug'.
+    Overwrite these fields as needed.
+
+    E.g.: 'Exercises to live by' --> 'exercises-to-live-by'
+    E.g.:    'A great hamburger' --> 'a-great-hamburger'
+    E.g.:    'A great hamburger' --> 'a-great-hamburger-2'
+    """
+    _slugify = 'title'
+    _slug_field = 'slug'
+
+    @staticmethod
+    def __get_number_of_digits(n: int) -> int:
+        return int(math.log10(n) + 1)
+
+    def __ensure_fields_exists(self):
+        error = "Field %(field)s does not exist on model " + self.__class__.__name__
+        if not hasattr(self, self._slugify):
+            raise FieldError(error % {'field': self._slugify})
+        if not hasattr(self, self._slug_field):
+            raise FieldError(error % {'field': self._slug_field})
+
+    def _generate_unique_slug(self):
+        self.__ensure_fields_exists()
+        base = slugify(getattr(self, self._slugify))
+        slug = base
+
+        counter = 1
+        max_length = self.__class__._meta.get_field(self._slug_field).max_length
+        while self.__class__.objects.filter(**{self._slug_field: slug}):
+            d = self.__get_number_of_digits(counter)
+            s = base[0:max_length - (d + 1)].rstrip('-')
+
+            slug = '%s-%s' % (s, counter)
+            counter += 1
+
+        setattr(self, self._slug_field, slug)
+
+    def save(self, *args, **kwargs):
+        if not getattr(self, self._slug_field, None):
+            self._generate_unique_slug()
+        super(SluggedMixin, self).save(*args, **kwargs)
